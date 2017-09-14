945d4730271a6b56c5d44daed39bb4d27dfd0627
lonewolf <nikhilkalige@gmail.com>
2017-09-13 18:14:21 -0700

    Updates to timer

--- src/timer.rs
+++ src/timer.rs
@@ -2,11 +2,12 @@
 
 use core::any::{Any, TypeId};
 use core::ops::Deref;
+use core::marker::PhantomData;
 
 use cast::{u16, u32};
 use hal;
 use nb::{self, Error};
-use stm32f411::{GPIOA, RCC, TIM3, TIM4, gpioa, tim3};
+use stm32f411::{GPIOA, TIM1, TIM3, TIM4, gpioa, tim3, tim1};
 
 /// Channel associated to a timer
 #[derive(Clone, Copy, Debug)]
@@ -21,77 +22,111 @@ pub enum Channel {
     _4,
 }
 
-/// IMPLEMENTATION DETAIL
-pub unsafe trait TIM: Deref<Target = tim3::RegisterBlock> {
-    /// IMPLEMENTATION DETAIL
-    type GPIO: Deref<Target = gpioa::RegisterBlock>;
+pub unsafe trait TIMBase {
+    fn init(&self, timeout: ::apb1::Ticks);
+    fn set_timeout(&self, timeout: ::apb1::Ticks);
 }
 
-unsafe impl TIM for TIM3 {
-    type GPIO = GPIOA;
-}
-
-unsafe impl TIM for TIM4 {
-    type GPIO = GPIOA;
-}
-
-/// `hal::Timer` implementation
-pub struct Timer<'a, T>(pub &'a T) where T: 'a;
-
-impl<'a, T> Clone for Timer<'a, T> {
-    fn clone(&self) -> Self {
-        *self
-    }
-}
-
-impl<'a, T> Copy for Timer<'a, T> {}
-
-impl<'a, T> Timer<'a, T>
-    where T: Any + TIM
-{
-    /// Initializes the timer with a periodic timeout of `frequency` Hz
-    ///
-    /// NOTE After initialization, the timer will be in the paused state.
-    pub fn init<P>(&self, period: P, rcc: &RCC)
-        where P: Into<::apb1::Ticks>
-    {
-        self.init_(period.into(), rcc)
-    }
-
-    fn init_(&self, timeout: ::apb1::Ticks, rcc: &RCC) {
-        let tim3 = self.0;
-
-        // Enable TIMx
-        if tim3.get_type_id() == TypeId::of::<TIM3>() {
-            rcc.apb1enr.modify(|_, w| w.tim3en().set_bit());
-        } else if tim3.get_type_id() == TypeId::of::<TIM4>() {
-            rcc.apb1enr.modify(|_, w| w.tim4en().set_bit());
-        }
-
+unsafe impl TIMBase for tim3::RegisterBlock {
+    fn init(&self, timeout: ::apb1::Ticks) {
         // Configure periodic update event
-        self._set_timeout(timeout);
+        self.set_timeout(timeout);
 
         // Continuous mode
-        tim3.cr1.write(|w| w.opm().clear_bit());
+        self.cr1.write(|w| w.opm().clear_bit());
 
         // Enable the update event interrupt
-        tim3.dier.modify(|_, w| w.uie().set_bit());
+        self.dier.modify(|_, w| w.uie().set_bit());
     }
 
-    fn _set_timeout(&self, timeout: ::apb1::Ticks) {
+    fn set_timeout(&self, timeout: ::apb1::Ticks) {
         let period = timeout.0;
 
         let psc = u16((period - 1) / (1 << 16)).unwrap();
         let arr = u16(period / u32(psc + 1)).unwrap();
         unsafe {
-            self.0.psc.write(|w| w.psc().bits(psc));
-            self.0.arr.write(|w| w.arr_l().bits(arr));
+            self.psc.write(|w| w.psc().bits(psc));
+            self.arr.write(|w| w.arr_l().bits(arr));
         }
     }
 }
 
-impl<'a, T> hal::Timer for Timer<'a, T>
-    where T: Any + TIM
+unsafe impl TIMBase for tim1::RegisterBlock {
+    fn init(&self, timeout: ::apb1::Ticks) {
+        self.set_timeout(timeout);
+        self.cr1.write(|w| w.opm().clear_bit());
+        self.dier.modify(|_, w| w.uie().set_bit());
+    }
+
+    fn set_timeout(&self, timeout: ::apb1::Ticks) {
+        let period = timeout.0;
+        let psc = u16((period - 1) / (1 << 16)).unwrap();
+        let arr = u16(period / u32(psc + 1)).unwrap();
+        unsafe {
+            self.psc.write(|w| w.psc().bits(psc));
+            self.arr.write(|w| w.arr().bits(arr));
+        }
+    }
+}
+
+pub unsafe trait TIM<T>: Deref<Target = T>
+    where T: TIMBase 
+{
+    /// IMPLEMENTATION DETAIL
+    type GPIO: Deref<Target = gpioa::RegisterBlock>;
+
+    fn init_(&self, timeout: ::apb1::Ticks) {
+        self.init(timeout);
+    }
+
+    fn set_timeout_(&self, timeout: ::apb1::Ticks) {
+        self.set_timeout(timeout);
+    }
+}
+
+unsafe impl TIM<tim3::RegisterBlock> for TIM3 {
+    type GPIO = GPIOA;
+}
+
+unsafe impl TIM<tim3::RegisterBlock> for TIM4 {
+    type GPIO = GPIOA;
+}
+
+unsafe impl TIM<tim1::RegisterBlock> for TIM1 {
+    type GPIO = GPIOA;
+}
+
+
+pub struct Timer<'a, T, R>(pub &'a T, pub PhantomData<R>) where T: 'a;
+
+// impl<'a, T, R> Clone for Timer<'a, T, R>
+//     where R: TIMBase, T: Any + TIM<R> 
+// {
+//     fn clone(&self) -> Self {
+//         *self
+//     }
+// }
+
+// impl<'a, T, R> Copy for Timer<'a, T, R> {}
+
+impl<'a, T, R> Timer<'a, T, R>
+    where R: TIMBase, T: Any + TIM<R>
+{
+    pub const fn new(tim: &'a T) -> Self {
+        Timer(tim, PhantomData)
+    }
+    /// Initializes the timer with a periodic timeout of `frequency` Hz
+    ///
+    /// NOTE After initialization, the timer will be in the paused state.
+    pub fn init<P>(&self, period: P)
+        where P: Into<::apb1::Ticks>
+    {
+        self.0.init_(period.into());
+    }
+}
+
+impl<'a, T> hal::Timer for Timer<'a, T, tim3::RegisterBlock>
+    where T: Any + TIM<tim3::RegisterBlock>
 {
     type Time = ::apb1::Ticks;
 
@@ -117,7 +152,47 @@ impl<'a, T> hal::Timer for Timer<'a, T>
     fn set_timeout<TO>(&self, timeout: TO)
         where TO: Into<::apb1::Ticks>
     {
-        self._set_timeout(timeout.into())
+        self.0.set_timeout_(timeout.into())
+    }
+
+    fn wait(&self) -> nb::Result<(), !> {
+        if self.0.sr.read().uif().bit_is_clear() {
+            Err(Error::WouldBlock)
+        } else {
+            self.0.sr.modify(|_, w| w.uif().clear_bit());
+            Ok(())
+        }
+    }
+}
+
+impl<'a, T> hal::Timer for Timer<'a, T, tim1::RegisterBlock>
+    where T: Any + TIM<tim1::RegisterBlock>
+{
+    type Time = ::apb1::Ticks;
+
+    fn get_timeout(&self) -> ::apb1::Ticks {
+        ::apb1::Ticks(u32(self.0.psc.read().psc().bits() + 1) * u32(self.0.arr.read().bits()))
+    }
+
+    fn pause(&self) {
+        self.0.cr1.modify(|_, w| w.cen().clear_bit());
+    }
+
+    fn restart(&self) {
+        unsafe {
+            self.0.cnt.modify(|_, w| w.bits(0));
+            self.0.cnt.write(|w| w.bits(0));
+        }
+    }
+
+    fn resume(&self) {
+        self.0.cr1.modify(|_, w| w.cen().set_bit());
+    }
+
+    fn set_timeout<TO>(&self, timeout: TO)
+        where TO: Into<::apb1::Ticks>
+    {
+        self.0.set_timeout_(timeout.into())
     }
 
     fn wait(&self) -> nb::Result<(), !> {
